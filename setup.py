fa313b7f47e665028f19a16b981dbd181ba60e4f
Gabriele Baldoni <gabriele.baldoni@gmail.com>
2016-08-01 10:24:19 +0200

    clean

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask==0.7.2', 'MarkupSafe' , 'Flask-SQLAlchemy==0.16'],
+setup(name='DG-API',
+      version='0.1',
+      description='Drive Green API',
+      author='Gabriele Baldoni',
+      author_email='gabriele.baldoni@gmail.com',
+      url='http://www.gabrielebaldoni.it',
+      install_requires=['Flask>=0.11.1', 'PyMongo>=3.3.0' , 'Flask-SQLAlchemy==0.16'],
      )
